21cf68a07bc4d5c6d436b65e7789b46495031313
Antonio Felix <anfelix@csumb.edu>
2019-05-02 03:07:28 +0000

    update

---
+++ api/login.php
@@ -0,0 +1,34 @@
+<?php
+    include '../connect.php';
+    session_start();
+
+    // Get Data from DB
+    $conn = getDatabaseConnection("QuickTube");
+    $namedParameters = array();
+  
+    $namedParameters[":username"] = $_GET['username'];
+  
+    $sql = "SELECT * FROM user " .
+        "WHERE username = :username ";
+  
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($namedParameters);
+    $record = $stmt->fetch();
+
+    if($_GET["password"] = $record["password"]){
+        $isAuthenticated = true;
+    }else {
+        $isAuthenticated = false;
+    }
+
+    
+    if ($isAuthenticated) {
+        $_SESSION["username"] = $record["username"];
+        $_SESSION["isAdmin"] = $record["is_admin"];
+    }
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+
+    echo json_encode(array("isAuthenticated" => $isAuthenticated));
+?>
