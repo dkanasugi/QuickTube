b06e6d4e6876936336ccd3bb690217117dd399ce
Ricky Baca <rbaca@csumb.edu>
2019-05-02 04:04:04 +0000

    fixed button

--- api/signUp.php
+++ api/signUp.php
@@ -2,10 +2,7 @@
     include '../connect';
     
     $conn = getDatabaseConnection("QuickTube");
-<<<<<<< HEAD
-=======
 
->>>>>>> 18d3ccc8250bc9391e45418006d802cb8712e60a
     $uName = $_POST['username'];
     $password = $_POST['password'];
     
